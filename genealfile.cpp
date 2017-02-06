bbbde2c8ca4cb8cb1fa87dbaadf899d786e3e781
Hannah von Reth <vonreth@kde.org>
2017-02-06 13:50:40 +0100

    Use QTextStream

--- genealfile.cpp
+++ genealfile.cpp
@@ -1,6 +1,7 @@
 #include "genealfile.h"
 
 #include <QFile>
+#include <QTextStream>
 
 GenealFile::GenealFile(QObject *parent)
     : QObject(parent)
@@ -8,26 +9,28 @@ GenealFile::GenealFile(QObject *parent)
 
 }
 
-QString GenealFile::readFile(const QUrl &file) const
+QString GenealFile::readFile(const QUrl &fileUrl) const
 {
     QString out;
 
-    QFile f(file.toLocalFile());
-    if (!f.open(QFile::ReadOnly))
-        qFatal("Failed to read %s", qPrintable(file.toLocalFile()));
-    out = QString::fromLocal8Bit(f.readAll());
-    f.close();
-
+    QFile file(fileUrl.toLocalFile());
+    if (!file.open(QFile::ReadOnly))
+        qFatal("Failed to read %s", qPrintable(fileUrl.toLocalFile()));
+    QTextStream stream(&file);
+    stream.setAutoDetectUnicode(true);
+    stream >> out;
+    file.close();
     return out;
 }
 
-void GenealFile::writeFile(const QUrl &file, const QString &text) const
+void GenealFile::writeFile(const QUrl &fileUrl, const QString &text) const
 {
-    QFile f(file.toLocalFile());
-    if (!f.open(QFile::WriteOnly))
-        qFatal("Failed to write %s", qPrintable(file.toLocalFile()));
-    f.write(text.toUtf8().constData());
-    f.close();
+    QFile file(fileUrl.toLocalFile());
+    if (!file.open(QFile::WriteOnly))
+        qFatal("Failed to write %s", qPrintable(fileUrl.toLocalFile()));
+    QTextStream stream(&file);
+    stream << text;
+    file.close();
 }
 
 bool GenealFile::fileExists(const QUrl &file) const
